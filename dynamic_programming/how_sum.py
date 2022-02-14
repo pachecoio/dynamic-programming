f44a20fa30a210c577b7373d04361e74584b6809
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2022-02-13 22:02:27 -0500

    Included docs to how_sum

--- dynamic_programming/how_sum.py
+++ dynamic_programming/how_sum.py
@@ -1,4 +1,23 @@
+"""
+Given a list of numbers (values) returns a combination of numbers that
+can sum up to a target number (n)
+
+E.g.
+    how_sum(8, [2, 3, 7]) -> [2, 2, 2, 2]
+"""
+
+
 def how_sum(n: int, values: [int], memo: dict = None) -> [int]:
+    """
+    Returns the combination of numbers from a given list (values)
+    that can sum up to a target number.
+
+    :param n: Target number
+    :param values: List of numbers to be checked
+    :param memo: Dictionary holding common repetitive results
+    :return: List of numbers
+    """
+
     if memo is None:
         memo = {}
     if n in memo:

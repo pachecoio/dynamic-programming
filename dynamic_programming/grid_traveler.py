a98264cd3397a5becbf76e2e6e3be2280c1b3deb
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2022-02-13 21:58:04 -0500

    Included docs for grid traveler

--- dynamic_programming/grid_traveler.py
+++ dynamic_programming/grid_traveler.py
@@ -1,4 +1,34 @@
+"""
+Given a grid find the amount of steps to go from the start cell to the last cell.
+
+E.g.
+    [
+        [start, 0 ,  0 ],
+        [  0  , 0 , end],
+    ]
+    get_step_count(2, 3) -> 3
+"""
+
+
 def get_step_count(rows: int, columns: int, memo: dict = None) -> int:
+    """
+    Returns the total count of the steps necessary to go from the start cell to the end cell
+    of a grid
+
+    Brute force complexity:
+    Time: O(2^(n+m))
+    Space: O(n + m)
+
+    Memoized complexity:
+    Time: O(n * m)
+    Space(n + m)
+
+
+    :param rows: int -> amount of rows
+    :param columns: int -> amount of columns
+    :param memo: dict -> dictionary holding common repetitive results
+    :return: int -> Total count
+    """
     if memo is None:
         memo = {}
     key = f'{rows},{columns}'
